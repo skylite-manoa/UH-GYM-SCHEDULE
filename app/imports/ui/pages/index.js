36a70f03631ea99a7bc9b6adeed12b3ac02ce9c4
Chris Yeager <cgyeager@hawaii.edu>
2017-04-27 19:06:58 -1000

    deleted stuff

---
+++ app/imports/ui/pages/index.js
@@ -0,0 +1,29 @@
+import './personal-page.html';
+import './personal-page.js';
+
+import './edit-personal-page.html';
+import './edit-personal-page.js';
+
+import './home-page.html';
+import './home-page.js';
+
+import './add-workout-page.html';
+import './add-workout-page.js';
+
+import './edit-workout-page.html';
+import './edit-workout-page.js';
+
+import './list-workout-page.html';
+import './list-workout-page.js';
+
+import './scheduler.html';
+import './scheduler.js';
+
+import './calendar-page.html';
+import './calendar-page.js';
+
+import './create-event-modal.html';
+import './create-event-modal.js';
+
+import './about.html';
+import './about.js';
